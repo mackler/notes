a81c49c0e025a451df56b63af7c31fbfc5e81a0d
Adam Mackler <AdamMackler@gmail.com>
2018-11-09 04:42:53 -0500

    Change name of publicly-exposed db-schema to "public"

--- example.sql
+++ example.sql
@@ -1,25 +1,25 @@
 -- http://postgrest.org/en/v5.1/tutorials/tut0.html#step-4-create-database-for-api
 
 -- create a named schema for the database objects which will be exposed in the API.
-create schema api;
+create schema public;
 
 -- API will have one endpoint, /todos, which will come from a table.
-create table api.todos (
+create table public.todos (
   id serial primary key,
   done boolean not null default false,
   task text not null,
   due timestamptz
 );
 
-insert into api.todos (task) values
+insert into public.todos (task) values
   ('finish tutorial 0'), ('pat self on back');
 
 -- make a role to use for anonymous web requests.
 create role web_anon nologin;
 grant web_anon to postgres;
 
-grant usage on schema api to web_anon;
--- grant select on api.todos to web_anon;
+grant usage on schema public to web_anon;
+-- grant select on public.todos to web_anon;
 
 -- make a role called notes_user for users who authenticate with the API.
 -- This role will have the authority to do anything to the todo list.
@@ -27,6 +27,6 @@ grant usage on schema api to web_anon;
 create role notes_user nologin;
 grant notes_user to postgres;
 
-grant usage on schema api to notes_user;
-grant all on api.todos to notes_user;
-grant usage, select on sequence api.todos_id_seq to notes_user;
+grant usage on schema public to notes_user;
+grant all on public.todos to notes_user;
+grant usage, select on sequence public.todos_id_seq to notes_user;
